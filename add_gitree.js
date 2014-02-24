f76c4f1214e03f45d4a012aec4e4ef397c93a49f
Elzair <elzairthesorcerer@gmail.com>
2014-02-24 16:20:12 -0500

    Moved example .gitree.json to gitree.json and fixed errors in add_gitree.js

--- add_gitree.js
+++ add_gitree.js
@@ -7,6 +7,30 @@ var exec   = require('child_process').exec
   , util   = require('util')
   , zlib   = require('zlib');
 
+var handle_error = function(err) {
+  if (err) {
+    console.error(JSON.stringify(err));
+    process.exit(1);
+  }
+};
+
+var write_blob = function(dir_path, buff, hash) {
+  var file_path = path.join(dir_path, hash.substring(2, hash.length));
+  fs.stat(file_path, function(err, stats) {
+    handle_error(err);
+    if (stats.isFile()) {
+      console.log('Warning: Identical .gitree.json already exists in repository!');
+      console.log(util.format("%s", hash));
+    }
+    else {
+      fs.writeFile(file_path, buff, function(err) {
+        handle_error(err);
+        console.log(util.format("%s", hash));
+      });
+    }
+  });
+};
+
 var main = function() {
   // Get directory to find .gitree.json file
   var cmdargs = process.argv.slice(2,process.argv.length);
@@ -15,10 +39,7 @@ var main = function() {
 
   // Read input file
   fs.readFile(pa, function(err, data) {
-    if (err) {
-      console.error(JSON.stringify(err));
-      return 1;
-    }
+    handle_error(err);
 
     // Create header of git blob object
     var header = new Buffer(util.format('blob %d\u0000', data.length));
@@ -36,24 +57,21 @@ var main = function() {
 
     // Compress object & store it in git repository
     zlib.deflate(store, function(err, buff) {
-      if (err) {
-        console.error(JSON.stringify(err));
-        return 1;
-      }
+      handle_error(err);
+
       var dir_path = path.join(cwd, '.git', 'objects', hash.substring(0,2));
-      fs.mkdir(dir_path, function(err) {
-        if (err) {
-          console.error(JSON.stringify(err));
-          return 1;
+      fs.stat(dir_path, function(err, stats) {
+        handle_error(err);
+
+        if (stats.isDirectory()) {
+          write_blob(dir_path, buff, hash);
         }
-        var file_path = path.join(dir_path, hash.substring(2, hash.length));
-        fs.writeFile(file_path, buff, function(err) {
-          if (err) {
-            console.error(JSON.stringify(err));
-            return 1;
-          }
-          console.log(util.format("%s", hash));
-        });
+        else {
+          fs.mkdir(dir_path, function(err) {
+            handle_error(err);
+            write_blob(dir_path, buff, hash);
+          });
+        }
       });
     });
   });
