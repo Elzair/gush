1be607a49373220bcb4eb61836454a719cc25fc5
Elzair <elzairthesorcerer@gmail.com>
2014-03-05 14:29:04 -0500

    Refactored most functions to specify options in an options object

--- add_gush.js
+++ add_gush.js
@@ -8,39 +8,22 @@ var exec       = require('child_process').exec
   , git        = require('./lib/git')
   ;
 
-/*
- * This function adds the .gush.json file in the current working directory to the git
- * repository in the same directory.
- * @param cwd path to git repository
- * @param gush_path directory containing .gush.json
- * @param cb callback function to execute
- */
-var add_gush_file = function(cwd, gush_path, cb) {
-  git.add_git_object(cwd, gush_path, cb);
-};
-
-
 /**
- * This function adds a tag to the current .gush.json file.
- * @param cwd path to git repository
- * @param hash SHA-1 hash of blob object containing current .gush.json file
+ * This function adds the gush config file to the git repository in the current working directory.
+ * @param options.cwd path to git repository
+ * @param options.path path to gush config file
+ * @param options.tag_name tag name
+ * @param options.tag_message tag message
+ * @param options.overwrite whether or not to overwrite existing tag
  * @param cb callback function to execute
  */
-var add_gush_tag  = function(cwd, hash, cb) {
-  git.add_tag_object(cwd, hash, 'gush_json', '.gush.json', function(error, stdout) {
-    // If tag already exists, delete it and tag the new object
-    if (error && error.hasOwnProperty('error') && error.error.hasOwnProperty('code') && error.error.code === 128) {
-      git.delete_tag_object(cwd, 'gush_json', function(inner_error, inner_stdout) {
-        err.handle_error(inner_error);
-        git.add_tag_object(cwd, hash, 'gush_json', '.gush.json', cb);
-      });
-    }
-    else if (error) {
-      err.handle_error(error);
-    }
-    else {
-      cb(error, stdout);
-    }
+var add_gush_config = function(options, cb) {
+  git.add_git_object(options.cwd, options.path, function(error, hash) {
+    options.hash = hash;
+    git.add_tag(options, function(inner_error, inner_stdout) {
+      var my_err = [error, inner_error].join("\n");
+      cb(my_err, inner_stdout);
+    }); 
   });
 };
 
@@ -50,24 +33,32 @@ var add_gush_tag  = function(cwd, hash, cb) {
 var main = function() {
   // Get path to gush config file
   var script_name = path.basename(process.argv[1]);
-  var cmdargs = process.argv.slice(3,process.argv.length);
-  var gush_path = cmdargs[0];
-  if (!gush_path) {
+  var cmdargs = process.argv.slice(2,process.argv.length);
+  if (cmdargs.length <= 2) {
     console.error(util.format("Usage: %s add /path/to/gush/config/file [/path/to/git/repository]", script_name));
     process.exit(1);
   }
-  var cwd = cmdargs[1] || process.cwd();
- 
-  // Get the hash of the current .gush.json file
-  add_gush_file(cwd, gush_path, function(error, hash) {
-    err.handle_error(error);
-    console.log('Added gush config file to repository!');
+  var cmd = cmdargs[0];
+  if (cmd !== 'add') {
+    console.error(util.format("Usage: %s add /path/to/gush/config/file [/path/to/git/repository]", script_name));
+    process.exit(1);
+  }
 
-    // Create a tag object for the current .gush.json file
-    add_gush_tag(cwd, hash, function(error, stdout) {
-      err.handle_error(error);
-      console.log("Successfully added & tagged current gush config file!");
-    });
+  // Get options
+  var options = {};
+  options.path = cmdargs[1];
+  options.cwd = cmdargs[2] || process.cwd();
+  options.tag_message = path.basename(options.path);
+  options.tag_name = options.tag_message.replace(/\./g, "_");
+  options.overwrite = true;
+  options.annotated = true;
+ 
+  add_gush_config(options, function(error, stdout) {
+    err.handle_error(error);
+    if (stdout !== undefined && stdout !== null && stdout !== '') {
+      console.log(stdout);
+    }
+    console.log("Successfully added & tagged current gush config file!");
   });
 };
 
